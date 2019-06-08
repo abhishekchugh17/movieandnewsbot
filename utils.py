fdf2ec75c74fba5c3b4735a4781be2653e04984b
abhishekchugh17 <abhishekchugh21@gmail.com>
2019-06-08 08:21:28 +0530

    maybefinal

--- utils.py
+++ utils.py
@@ -2,7 +2,11 @@ import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "moviekey.json"
 
 import tmdbsimple as tmdb
+from tmdbv3api import TMDb
+tmdb2 = TMDb()
+tmdb2.api_key = '1422b384f2b0eda87bcf3ceecc6d0df7'
 tmdb.API_KEY = '1422b384f2b0eda87bcf3ceecc6d0df7'
+from tmdbv3api import Movie
 
 import dialogflow_v2 as dialogflow
 dialogflow_session_client = dialogflow.SessionsClient()
@@ -30,20 +34,30 @@ def detect_intent_from_text(text, session_id, language_code='en'):
 
 def fetch_reply(msg, session_id):
     response = detect_intent_from_text(msg, session_id)
-    #print(response.parameters)
-    print(dict(response.parameters).get('get_fav').values)
+    print("############################",response.parameters)
 
-    if dict(response.parameters).get('get_money') != '':
-        cost_info = get_cost(response.parameters)
-        return cost_info
-    elif response.intent.display_name == 'get_review':
-        #print('###############################',dict(response.parameters))
+    if response.intent.display_name == 'get_overview':
         movie_info = get_info(response.parameters)
-        if (len(dict(response.parameters).get('get_fav').values) > 0 ):
+        if dict(response.parameters)['get_money'] != '':
+            cost_info = get_cost(response.parameters)
+            return "*Production Cost :* " + cost_info
+        if len(dict(response.parameters).get('get_fav')) > 0 :
             upload(msg, session_id,movie_info,True)
         else:
             upload(msg,session_id,movie_info)
         return str(movie_info)
+
+    elif response.intent.display_name == 'get_news':
+        news = get_news(dict(response.parameters))
+        news_str = 'Here is your news : '
+        for row in news:
+            news_str += "\n\n{}\n\n{}".format(row['title'],row['link'])
+        print(news_str)
+        return str(news_str)[:400]
+    
+    elif response.intent.display_name == 'get_recommendation':
+        recc = get_recommend(response.parameters)
+        return recc
 
     else:
         return response.fulfillment_text
@@ -63,4 +77,15 @@ def get_info(parameters):
     response = search.movie(query=parameters['movie_name'])
     ans = str("Title : {}\n Release date : {} \nOverview : {} \nPopularity : {}".format(response['results'][0]['title'],response['results'][0]['release_date']
                     ,response['results'][0]['overview'],response['results'][0]['popularity']))
+    return ans
+
+def get_recommend(parameters):
+    movie = Movie()
+    search = tmdb.Search()
+    response = search.movie(query=parameters['movie_name'])
+    idd = response['results'][0]['id']
+    recommendations = movie.recommendations(idd)
+    ans = ''
+    for recommendation in recommendations:
+        ans += (recommendation.title+"\n")
     return ans
